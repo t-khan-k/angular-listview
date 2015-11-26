feb4ae57707e129b1775f4773fb0872e5c344e20
t-khan-k <tkssuetce@live.com>
2015-11-26 18:28:35 +0500

    tk repeat done

--- tk-repeat.js
+++ tk-repeat.js
@@ -9,17 +9,16 @@ angular.module('app').directive('listView',function($compile){
 			var exp = attr.listView.split(' in ');
 			var child = exp[0];
 			var data = scope.$eval(exp[1]);
-			var template = elem[0].outerHTML;
+			var template = elem[0].innerHTML;
+			elem.html("");
 			insertElements(data,template);
-			console.log(template);
 
 			function insertElements(data,template){
-				var e = [];
 				for(var i=0;i<data.length;i++){
 					var el = angular.element(template);
 					var childScope = createChildScope(child,data[i]);
-					//$compile(el)(childScope);
-					//$compile(el)(scope).insertAfter(elem);
+					//$compile(el)(childScope).insertBefore(elem);
+					elem.append($compile(el)(childScope))
 				}
 			}
 
@@ -28,17 +27,6 @@ angular.module('app').directive('listView',function($compile){
 				cs[child] = row;
 				return cs;
 			}
-
-			/*var arr = [];
-			var html = elem.children()[0].innerHTML;
-			elem.html("");
-			for(var i=0;i<scope.src.length;i++){
-				scope.name = scope.src[i].name;
-				scope.age = scope.src[i].age;
-				var el = angular.element(html);
-				arr = arr.concat($compile(el)(scope));
-				arr[i].insertAfter(elem);
-			}*/
 		}
 	}
 });
